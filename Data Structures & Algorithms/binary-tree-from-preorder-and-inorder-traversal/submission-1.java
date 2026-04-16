638e75c0c679b2f947ea539623b375941b928ee1
Etisha Mathurvaishya <etisha.mathur@gmail.com>
2026-04-16 22:14:20 +0530

    Add: binary-tree-from-preorder-and-inorder-traversal - submission-1

---
+++ Data Structures & Algorithms/binary-tree-from-preorder-and-inorder-traversal/submission-1.java
@@ -0,0 +1,40 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+
+class Solution {
+    int[] preorder;
+    int[] inorder;
+    public TreeNode buildTree(int[] preorder, int[] inorder) {
+        this.preorder = preorder;
+        this.inorder = inorder;
+        return build(0, preorder.length-1, 0, inorder.length-1);
+    }
+    private TreeNode build(int preStart,int preEnd,int inStart,int inEnd){
+        if(preStart > preEnd || inStart > inEnd)
+            return null;
+        TreeNode node = new TreeNode(preorder[preStart]);
+        int i = 0;
+        for(i =inStart; i<inEnd; i++)
+        {
+            if(inorder[i] == node.val)
+                break;
+        }
+        int leftSize = i - inStart;
+        node.left=build(preStart+1, preStart+leftSize, inStart, i-1);
+        node.right=build(preStart+leftSize+1, preEnd, i+1, inEnd);
+        return node;
+    }
+}
